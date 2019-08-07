6f1152ce61f79b4c4e2cdf3bc56e8ea0c79bdd35
chengshaohui <1617487191@qq.com>
2019-08-07 15:55:36 +0800

    cube-ui

---
+++ src/components/header-detail/header-detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-detail">
+    header-detail
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header-detail',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="stylus"></style>
